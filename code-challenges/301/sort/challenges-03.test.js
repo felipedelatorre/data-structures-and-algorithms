d825dd6abaebdce4888289f14257d469c99862c3
felipedelatorre <felipede_latorre@hotmail.com>
2019-04-24 23:07:34 -0700

    Challenge 5 done

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -43,7 +43,10 @@ For example, ['Alphabet', 'alphabet', 'carrot', 'Zebra'] is correctly sorted, an
 ------------------------------------------------------------------------------------------------ */
 
 const alphabetizeBetter = (arr) => {
-  // Solution code here...
+  arr.sort( (a, b ) => {
+    return a.toUpperCase() > b.toUpperCase();
+  })
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -58,7 +61,10 @@ Here is an example of the input:
 ------------------------------------------------------------------------------------------------ */
 
 const sortByPrice = (arr) => {
-  // Solution code here...
+  arr.sort( (a, b) => {
+    return a.price - b.price;
+  })
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
