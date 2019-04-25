cd2d508f0086736e9a12dc9b20eb8c4060a65ee8
felipedelatorre <felipede_latorre@hotmail.com>
2019-04-24 23:22:07 -0700

    Challenge 7 done

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -74,7 +74,10 @@ For example, [1, 14, 0.2, -281, 54782] is only correctly sorted in that order.
 ------------------------------------------------------------------------------------------------ */
 
 const sortNumbersByLength = (arr) => {
-  // Solution code here...
+  arr.sort( (a, b) => {
+    return a.toString().length > b.toString().length;
+  })
+  return arr;
 };
 
 /*-----------------------------------------------------------------------------------------------
@@ -95,7 +98,10 @@ const people = [
 ];
 
 const sortPeople = (arr) => {
-  // Solution code here...
+  arr.sort( (a, b) => {
+    return a.lastName > b.lastName;
+  })
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
