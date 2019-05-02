c4fd201c257efc9ffdb6fc784ea3e941ab6a3110
felipedelatorre <felipede_latorre@hotmail.com>
2019-05-01 18:14:03 -0700

    challenge 6 done

--- code-challenges/301/arrayFilter/challenges-08.test.js
+++ code-challenges/301/arrayFilter/challenges-08.test.js
@@ -147,7 +147,11 @@ const characters = [
 ];
 
 const getCharactersWithoutChildren = (arr) => {
-  // Solution code here...
+  let newArray = arr.filter(element => {
+    if (!element.children)
+      return element;
+  });
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
