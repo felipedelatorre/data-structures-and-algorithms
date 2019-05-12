109560c2a0f3d4b29b6013d29d675da48939b079
felipedelatorre <felipede_latorre@hotmail.com>
2019-05-12 13:31:55 -0700

    Challenge 1 done

--- code-challenges/301/chaining/challenges-10.test.js
+++ code-challenges/301/chaining/challenges-10.test.js
@@ -8,8 +8,12 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
-  
-
+  return input.reduce((accumulator, arr) => {
+    accumulator += arr.reduce((acc, int) => {
+      return target === int ? acc += 1 : acc;
+    }, 0);
+    return accumulator;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -19,21 +23,15 @@ You may want to use filter, map, or reduce for this problem, but are not require
 For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
-const totalSum = (input) => {
-  // Solution code here...
-};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 Write a function named divisibleByFiveTwoToThePower that accepts an array of arrays as input.
-This function should first remove any elements that are not numbers or are not divisible by five.
+This function should first remove any elements that are not numbers or are not divisible by five.~
 This function should then raise 2 to the power of the resulting numbers, returning an array of arrays.
 For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
-const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
-};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -94,18 +92,19 @@ let starWarsData = [{
   gender: 'female'
 }];
 
-let findMaleAndFemale = (data) => {
-  // Solution code here...
-};
+
+// Daniel Breoti's function 
+//starWarsData.filter((char) => char.gender !== 'n/a').map((char) => char.name)
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
 Write a function named findShortest that, given the Star Wars data from Challenge 6, uses any combination of filter, map and reduce to return the name of the shortest character.
 ------------------------------------------------------------------------------------------------ */
 
-let findShortest = (data) => {
-  // Solution code here...
-};
+// My function
+
+//// Daniel Breoti's function
+// starWarsData.sort((a, b) => a.height - b.height).shift().name;
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
