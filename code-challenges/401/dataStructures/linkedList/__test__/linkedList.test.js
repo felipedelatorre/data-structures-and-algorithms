fcffe1988a0e747964c761972254452e7daeaa3d
felipedelatorre <felipede_latorre@hotmail.com>
2019-06-03 13:34:45 -0700

    done with challenges

--- code-challenges/401/dataStructures/linkedList/__test__/linkedList.test.js
+++ code-challenges/401/dataStructures/linkedList/__test__/linkedList.test.js
@@ -4,33 +4,105 @@ const LinkedList = require('../linkedList');
 
 describe('Testing with Linkedlist ', () => {
 
-  it('should create a linked list. Head pointing to null', () => {
-    let list = new LinkedList();
-    expect(list.head).toEqual(null);
+  let list;
+
+  beforeEach(() => {
+    list = new LinkedList();
   });
 
-  it('after adding a new node with value of 5, head should not be null. Head.value should be equals to 5.', () => {
-    let list = new LinkedList();
-    list.insert(5);
-    expect(list.head.value).toEqual(5);
+
+  describe('Instance of LinkedList', () => {
+
+    it('should create a linked list. Head pointing to null', () => {
+      expect(list.head).toEqual(null);
+    });
+
   });
 
-  it('should create a new node after the first. The first node.next should point towards the second and the second.next should point towards null', () => {
-    let list = new LinkedList();
-    list.insert(5);
-    list.append(6);
-    expect(list.head.next.value).toEqual(6);
-    expect(list.head.next.next).toEqual(null);
+  describe('insert()', () => {
+
+    it('after adding a new node with value of 5, head should not be null. Head.value should be equals to 5.', () => {
+      list.insert(5);
+      expect(list.head.value).toEqual(5);
+    });
+
+    it('should add a new value to the head and push old values to the end of the list', () => {
+      list.insert(5);
+      list.insert(10);
+      list.insert(30);
+
+      expect(list.head.value).toEqual(30);
+      expect(list.head.next.next.value).toEqual(5);
+    });
   });
 
-  it('should return true or false based on if the value is included in the list', () => {
-    let list = new LinkedList();
-    list.insert(5);
-    list.append(6);
-    list.append(7);
-    expect(list.includes(4)).toEqual(false);
-    expect(list.includes(5)).toEqual(true);
+  describe('append()', () => {
+
+    it('Can successfully add a node to the end of the linked list', () => {
+      list.insert(5);
+      list.append(6);
+      expect(list.head.next.value).toEqual(6);
+      expect(list.head.next.next).toEqual(null);
+    });
+
+    it('Can successfully add multiple nodes to the end of a linked list', () => {
+      list.insert(5);
+      list.append(6);
+      list.append(7);
+      expect(list.includes(4)).toEqual(false);
+      expect(list.includes(5)).toEqual(true);
+    });
+
   });
+
+
+  describe('insertBefore()', () => {
+    it('Can successfully insert a node before a node located in the middle of a linked list', () => {
+      list.insert(1);
+      list.append(2);
+      list.append(4);
+      list.append(5);
+      list.insertBefore(4, 3);
+
+      expect(list.includes(3)).toEqual(true);
+    });
+
+    it('Can successfully insert a node before the first node of a linked list', () => {
+      list.insert(1);
+      list.append(2);
+      list.append(4);
+      list.append(5);
+      list.insertBefore(1, 0);
+
+      expect(list.includes(0)).toEqual(true);
+    });
+  });
+
+  describe('insertAfter()', () => {
+    it('Can successfully insert after a node in the middle of the linked list', () => {
+      list.insert(1);
+      list.append(2);
+      list.append(4);
+      list.append(5);
+      list.insertAfter(2, 3);
+
+      expect(list.includes(3)).toEqual(true);
+    });
+
+    it('Can successfully insert a node after the last node of the linked list', () => {
+      list.insert(1);
+      list.append(2);
+      list.append(4);
+      list.append(5);
+      list.insertAfter(5, 6);
+
+      expect(list.includes(6)).toEqual(true);
+    });
+
+  });
+
+
+
 
 
 
