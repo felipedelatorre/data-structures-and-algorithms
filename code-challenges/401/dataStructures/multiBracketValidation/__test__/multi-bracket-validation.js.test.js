c2e847bdcd73e992a2250c6af049f34d4ac5754f
felipedelatorre <felipede_latorre@hotmail.com>
2019-06-12 13:16:23 -0700

    scafold

---
+++ code-challenges/401/dataStructures/multiBracketValidation/__test__/multi-bracket-validation.js.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const multiBracketValidation = require('../multi-bracket-validation.js');
+
+describe('nameOfChallenge\'s Description ', () => {
+
+  it('', () => {
+    expect().toEqual();
+  });
+
+
+
+
+});
---
+++ code-challenges/401/dataStructures/multiBracketValidation/multi-bracket-validation.js.js
@@ -0,0 +1,29 @@
+function multiBracketValidation(string){
+  let brackets = ['(',')','[',']','{','}',]
+  let stringArr = string.split('');
+  let bracketArr = [];
+
+  
+  for(let i = 0; i < stringArr.length; i++){
+    if (brackets.includes(stringArr[i])){
+      bracketArr.push(stringArr[i]);
+    }
+  }
+  console.log(bracketArr);
+  if(bracketArr.length % 2 === 1){
+    return false;
+  }
+
+  let end = bracketArr.length;
+
+  for(let start = 0; start <= end; start++){
+    if(bracketArr[start] !== bracketArr[end]){
+      return false;
+    } else{
+      end--;
+    }
+  }
+  return true;
+}
+
+module.exports = multiBracketValidation;
