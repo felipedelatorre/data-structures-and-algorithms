fa070b1f92fcdbf44d1720987f6a8690053b4c17
felipedelatorre <felipede_latorre@hotmail.com>
2019-06-26 03:15:20 -0700

    fizzBuzz resubmit

--- code-challenges/401/dataStructures/fizzBuzzTree/__test__/fizz-buzz-tree.test.js
+++ code-challenges/401/dataStructures/fizzBuzzTree/__test__/fizz-buzz-tree.test.js
@@ -9,13 +9,18 @@ describe('fizzBuzz(treeObj) ', () => {
 
   beforeEach(() => {
     let root = new Node(15);
+    let two = new Node(2);
     let five = new Node(5);
     let eighteen = new Node(18);
 
     root.left = five;
     root.right = eighteen;
 
+    five.left = two;
 
+    //      15
+    //    5   18
+    //   2
     bst.root = root;
   });
 
@@ -23,14 +28,20 @@ describe('fizzBuzz(treeObj) ', () => {
     expect(bst).toBeInstanceOf(BST);
   });
 
-  it('should change value of root.left from 5 to Fizz', () => {
+  it('should change value of root.left from 5 to Buzz', () => {
     fizzBuzzTree(bst);
-    expect(bst.root.left.value).toBe('Fizz');
+    expect(bst.root.left.value).toBe('Buzz');
   });
 
-  it('should change value of root.right from 18 to Buzz', () => {
+  it('should change value of root.right from 18 to Fizz', () => {
     fizzBuzzTree(bst);
-    expect(bst.root.right.value).toBe('Buzz');
+    expect(bst.root.right.value).toBe('Fizz');
   });
+
+  it('should not change value of root.left.left', () => {
+    fizzBuzzTree(bst);
+    expect(bst.root.left.left.value).toBe(2);
+  });
+
 
 });
