8d13bb0803e8d383feade24f75a2e779bf50a9d9
felipedelatorre <felipede_latorre@hotmail.com>
2019-07-06 21:39:36 -0700

    complete challenge

--- code-challenges/401/dataStructures/hashTable/__tests__/hashTable.test.js
+++ code-challenges/401/dataStructures/hashTable/__tests__/hashTable.test.js
@@ -1,64 +1,142 @@
 'use strict';
 
-const HashTable = require('../hashTable');
+const { HashTable } =require('../hashTable');
 
-describe('HashTable', () => {
+describe('Hash Table', () => {
+  it('should construct a Hash Table', () => {
+    //Arrange
+    let hashTable;
+    let expectedSize = 42;
 
-  it('', () => {
-    let hashTable = new HashTable;
+    //Act
+    hashTable = new HashTable(expectedSize);
+
+    //Assert
     expect(hashTable).toBeInstanceOf(HashTable);
+    expect(hashTable.size).toBeDefined();
+    expect(hashTable.size).toBe(expectedSize);
+    expect(hashTable.buckets).toBeDefined();
+    expect(hashTable.buckets.length).toBe(expectedSize);
+  });
+});
+
+describe('hash(key) function', () => {
+  it('should hash a key, cat, to 3 to an in-range value of 5 buckets ', () => {
+    //Arrange
+    let expectedHash = 3;
+    let key = 'cat';
+    let hashTable = new HashTable(5);
+
+    //Act
+    let result = hashTable.hash(key);
+
+    //Assert
+    expect(result).toBe(expectedHash);
   });
 
+  it('should hash foo to 1 with 5 buckets', () => {
+    //Arrange
+    let expectedHash = 1;
+    let key = 'foo';
+    let hashTable = new HashTable(5);
 
-  describe('hash(key)', () => {
-    it('should hash cat to 3 with 5 buckets', () => {
-      let expectedHash = 3;
-      let key = 'cat';
-      let hashTable = new HashTable(5);
+    //Act
+    let result = hashTable.hash(key);
 
-      let result = hashTable.hash(key);
+    //Arrange
+    expect(result).toBe(expectedHash);
+  });
 
-      expect(result).toBe(expectedHash);
-    });
+});
 
-    it('should hash foo to 1 with 5 buckets', () => {
-      let expectedHash = 1;
-      let key = 'foo';
-      let hashTable = new HashTable(5);
+describe('add(key, value)', () => {
+  it('should add a key/value to the hashtable resulting in the value being in the data structure', () => {
+    let hashTable = new HashTable(5);
+    let key = 'pig';
+    let value = 'round';
+    let key1 = 'foo';
+    let value1 = 55;
 
-      let result = hashTable.hash(key);
+    hashTable.add(key, value);
+    hashTable.add(key1, value1);
 
-      expect(result).toBe(expectedHash);
-    });
+    expect(hashTable.buckets[0].values()).toEqual([['pig', 'round']]);
+    expect(hashTable.buckets[1].values()).toEqual([['foo', 55]]);
 
   });
 
-  // it('Adding a key/value to your hashtable results in the value being in the data structure', () => {
+  it('should throw an error if the key is already being used', () => {
+    let hashTable = new HashTable(5);
+    let key = 'pig';
+    let value = 'round';
+    let key1 = 'pig';
+    let value1 = 55;
 
-  // });
+    hashTable.add(key, value);
+    expect( () => {
+      hashTable.add(key1, value1);
+    }).toThrow('Key already being used');
+  });
 
-  // it('Retrieving based on a key returns the value stored', () => {
+  it('should throw an error if no key is provided', () => {
+    let hashTable = new HashTable(5);
+    let key = '';
+    let value = 'round';
+    expect( () => {
+      hashTable.add(key, value);
+    }).toThrow('Invalid key provided');
+  });
 
-  // });
+});
 
-  // it('Successfully returns null for a key that does not exist in the hashtable', () => {
+describe('get(key)', () => {
+  it('should retrieve based on a key returns the value stored', () => {
+    let key = 'horse';
+    let value = 'brown';
+    let hashTable = new HashTable(5);
 
-  // });
+    hashTable.add(key, value);
 
-  // it('Successfully handle a collision within the hashtable', () => {
+    let result = hashTable.get(key);
 
-  // });
+    expect(result).toBe(value);
 
-  // it('Successfully retrieve a value from a bucket within the hashtable that has a collision', () => {
+  });
 
-  // });
+  it('should successfully throws an error for a key that does not exist in the hashtable', () => {
+    let badKey = '';
+    let hashTable = new HashTable(5);
+    expect( () => {
+      hashTable.get(badKey);
+    }).toThrow('Invalid key provided');
+  });
 
-  // it('Successfully hash a key to an in-range value', () => {
+});
 
-  // });
+describe('Collision handling', () => {
+  it('should successfully retrieve a value from a bucket within the hashtable that has a collision', () => {
+    let hashTable = new HashTable(5);
+    let key = 'cat';
+    let value = 42;
+    let key1 = 'bbt';
+    let value1 = 25;
 
+    hashTable.add(key, value);
+    hashTable.add(key1, value1);
 
+    expect(hashTable.buckets[3].values()).toEqual([['cat', 42], ['bbt', 25]]);
+  });
 
+  it('should successfully handle a collision within the hashtable', () => {
+    let hashTable = new HashTable(5);
+    let key = 'cat';
+    let value = 42;
+    let key1 = 'bbt';
+    let value1 = 25;
 
+    hashTable.add(key, value);
+    hashTable.add(key1, value1);
 
+    expect(hashTable.buckets[3].values()).toEqual([['cat', 42], ['bbt', 25]]);
+  });
 });
--- code-challenges/401/dataStructures/hashTable/hashTable.js
+++ code-challenges/401/dataStructures/hashTable/hashTable.js
@@ -7,12 +7,51 @@ class Node {
   }
 }
 
-class LinkedList {
+class LinkedList{
+  constructor(){
+    this.head = null;
+  }
 
+  add(value){
+    if(!value) throw new Error('Invalid value');
+
+    const node = new Node(value);
+
+    if(!this.head){
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+    while(current.next){
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  getWithKey(key){
+    if(!this.head) return;
+
+    let current = this.head;
+
+    while(current){
+      if(current.value[0] === key) return current.value[1];
+      current = current.next;
+    }
+    return;
+  }
+
+  values() {
+    let values = [];
+    let current = this.head;
+
+    while(current){
+      values.push(current.value);
+      current = current.next;
+    }
+    return values;
+  }
 }
-
-
-
 
 
 class HashTable {
@@ -22,41 +61,54 @@ class HashTable {
   }
 
   hash(key){
-    return key.split('').reduce((prev, curr) => prev + curr.charCodeAt(),0) * 599 % this.size;
+    return key.split('').reduce((prev, curr) => prev + curr.charCodeAt(0), 0) * 599 % this.size;
   }
 
   add(key, value){
     if(!key) throw new Error('Invalid key provided');
+    if(this.contains(key)) throw new Error('Key already being used');
+
     let index = this.hash(key);
-    if(!this.buckets[index]) {this.buckets[index] = new LinkedList();}
+
+    if(!this.buckets[index]){ this.buckets[index] = new LinkedList(); }
 
     try{
+      //this is referencing a LinkedList
       this.buckets[index].add([key, value]);
-    } catch(e){
+    }catch(e){
       throw e;
     }
   }
 
   get(key){
     if(!key) throw new Error('Invalid key provided');
+
     let index = this.hash(key);
-    if(!this.buckets[index]) {return null;}
+
+    if(!this.buckets[index]){ return null; }
 
     return this.buckets[index].getWithKey(key);
+
   }
 
   contains(key){
     if(!key) throw new Error('Invalid key provided');
+
     let index = this.hash(key);
-    if(!this.buckets[index]) {return null;}
+
+    if(!this.buckets[index]){ return null; }
 
     return this.buckets[index].getWithKey(key) ? true : false;
+
   }
 
+  // delete(){
+  //   //stretch goal
+  // }
 
-
-
+  prettyPrint() {
+    this.buckets.forEach( (item, index) => console.log(index, item && item.values()));
+  }
 }
 
-
-module.exports = HashTable;
+module.exports = { LinkedList, HashTable };
